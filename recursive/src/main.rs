f3340f1b838f9ef45ad7d32bab84c886a2547de0
Guocork <g1024536444@gmail.com>
2024-04-27 22:10:08 +0800

    斐波那契数列

--- recursive/src/main.rs
+++ recursive/src/main.rs
@@ -1,3 +1,7 @@
+/**
+ *  这里展示递归
+ */
+
 fn factorial(n: usize) -> usize {
     if n<=1 {
         return 1;
@@ -6,6 +10,14 @@ fn factorial(n: usize) -> usize {
     }
 }
 
+// 斐波那契数列
+// f(n) = f(n-1) + f(n-2)
+// f(0) = 0    f(1) = 1
+fn fibonacci(n: usize) -> usize {
+    return if n <= 2 { 1 } else { fibonacci(n - 1) + fibonacci(n - 2 ) };
+}
+
 fn main() {
     println!("{}",factorial(5));
+    println!("{}",fibonacci(5))
 }
