9b287866f8164b43a28a04431c7c4bd5d8d1be96
Guocork <g1024536444@gmail.com>
2024-04-29 18:21:52 +0800

    插入排序的两种实现（未优化）

--- insertion_sort/src/lib.rs
+++ insertion_sort/src/lib.rs
@@ -1,4 +1,7 @@
 
+/**
+ *  插入排序
+ */
 fn insertion_sort<T: Ord + std::fmt::Debug + Copy>(arr: &mut [T]) {
     for i in 1..arr.len() {
         let (mut p, v) = (i, arr[i]);
@@ -10,12 +13,41 @@ fn insertion_sort<T: Ord + std::fmt::Debug + Copy>(arr: &mut [T]) {
     }
 }
 
+fn insertion_sort2<T>(arr: &[T]) -> Vec<T> 
+where 
+    T: Ord + Clone + std::fmt::Debug
+{
+    let mut res = Vec::with_capacity(arr.len());
+    for item in arr.iter().cloned() {
+        let n_len = res.len();
+        for i in 0..=n_len {
+            if i == n_len || res[i] > item {
+                res.insert(i, item);
+                println!("{:?}",res);
+                break;
+            }
+        }
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        
+        let mut vec1 = vec![4,5,3,6,9,12];
+        insertion_sort(&mut vec1);
+        for i in 0..vec1.len() -1 {
+            assert!(vec1[i] <= vec1[i + 1]);
+        }
+    }
+
+    #[test]
+    fn insertion() {
+        let vec = vec![4,5,6,3,2,1,];
+        let res = insertion_sort2(&vec);
+        assert_eq!(res,vec![1,2,3,4,5,6]);
     }
 }
